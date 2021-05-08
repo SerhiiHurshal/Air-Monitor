c1660e0fa9a561f583b6deb32a8511a62c3d2ae7
Sergiy <seergiygurshall@gmail.com>
2021-05-08 11:31:09 +0300

    feat(root): add typescript, prettier and ESLint to app

---
+++ pages/index.tsx
@@ -0,0 +1,17 @@
+import Head from 'next/head';
+import React, { Fragment } from 'react';
+
+const Home = () => {
+  return (
+    <Fragment>
+      <Head>
+        <title>Air monitor</title>
+      </Head>
+      <main>
+        <h1>Air monitor</h1>
+      </main>
+    </Fragment>
+  );
+};
+
+export default Home;
