77de7e98b390e7ab56ea68f0870645be91bf1cd5
Sergiy <seergiygurshall@gmail.com>
2021-05-08 08:31:24 +0300

    feat(styles): add scss loader and normalize css

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
-import styles from '../styles/Home.module.css'
-
-export default function Home() {
-  return <div className={styles.container}></div>
+const Home = () => {
+  return <div>Hello Next</div>
 }
+
+export default Home
