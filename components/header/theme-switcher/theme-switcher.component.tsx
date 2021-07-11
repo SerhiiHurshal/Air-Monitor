49c1ac0747a9aec3325dee96f2ee1c0bc5fd1cd8
Sergiy <seergiygurshall@gmail.com>
2021-07-11 16:22:13 +0300

    refactor(theme-switcher): remove unnecessary code

--- components/header/theme-switcher/theme-switcher.component.tsx
+++ components/header/theme-switcher/theme-switcher.component.tsx
@@ -1,7 +1,6 @@
 import { Theme } from '@models/client';
 import classNames from 'classnames';
 import styles from './theme-switcher.module.scss';
-import Image from 'next/image';
 
 import moonIcon from '@images/theme-icons/moon.png';
 import sunIcon from '@images/theme-icons/sun.png';
@@ -12,30 +11,6 @@ interface Props {
 }
 
 const ThemeSwitcherComponent = ({ theme, onThemeSwitch }: Props) => (
-  // <div className={styles.container}>
-  //   <button
-  //     aria-label='button to switch theme'
-  //     className={classNames(
-  //       styles.button,
-  //       {
-  //         [styles.buttonLight]: theme === Theme.light,
-  //       },
-  //       {
-  //         [styles.buttonDark]: theme !== Theme.light,
-  //       },
-  //     )}
-  //     onClick={onThemeSwitch}
-  //   >
-  //     <Image
-  //       src={theme === Theme.light ? sunIcon : moonIcon}
-  //       alt='theme icon'
-  //       width={30}
-  //       height={30}
-  //     />
-  //   </button>
-  //   <div className={styles.line} />
-  // </div>
-
   <label className={styles.switch}>
     <input
       type='checkbox'
